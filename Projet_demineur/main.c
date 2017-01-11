71d1fa78e9bac1aff9026cc8580bd9e609be75c2
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2017-01-11 15:11:07 +0100

    Ajout du chrono

--- Projet_demineur/main.c
+++ Projet_demineur/main.c
@@ -9,17 +9,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 20
-#define BOMBES 20
+#define N 3
+#define BOMBES 1
 
 void afficher_tableau_entier(int a[N][N]);
 void afficher_tableau_char(char a[N][N]);
+void decouvrir(int tableau_complet[N][N], char tableau_user[N][N], int a, int b);
+void decouvrir_mines(int tableau_complet[N][N], char tableau_user[N][N]);
+
 
 int main(int argc, const char * argv[]) {
     int tableau_complet[N][N];
     int bombes_placees = 0, max_colonne = 0, max_ligne = 0, x, y, tentative = 0, perdu = 0;
     char tableau_user[N][N];
     time_t t;
+    time_t t0 = time(NULL);
     srand((unsigned) time(&t));
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
@@ -57,13 +61,15 @@ int main(int argc, const char * argv[]) {
             }
         }
     }
-    afficher_tableau_char(tableau_user);
+    afficher_tableau_entier(tableau_complet); // Debug
+    //afficher_tableau_char(tableau_user);
     do {
         printf("Quelle case souhaitez-vous découvrir ?\n");
         scanf("%d %d", &x, &y);
         switch (tableau_complet[x][y]) {
             case 0:
                 tableau_user[x][y] = '0';
+                //decouvrir(tableau_complet, tableau_user, x, y);
                 break;
             case 1:
                 tableau_user[x][y] = '1';
@@ -90,7 +96,7 @@ int main(int argc, const char * argv[]) {
                 tableau_user[x][y] = '8';
                 break;
             case 9:
-                tableau_user[x][y] = 'O';
+                decouvrir_mines(tableau_complet, tableau_user);
                 perdu = 1;
             default:
                 break;
@@ -102,10 +108,8 @@ int main(int argc, const char * argv[]) {
         //printf("\n\n");
     } while ((N*N) - tentative != BOMBES && perdu != 1);
     
-    if(perdu == 0)printf("Félicitations, vous avez gagné !\n");
+    if(perdu == 0)printf("Félicitations, vous avez gagné en %ld secondes !\n", (time(NULL)-t0));
     else printf("Vous avez perdu :(\n");
-    //afficher_tableau_entier(tableau_complet);
-    //afficher_tableau_char(tableau_user);
     return 0;
 }
 
@@ -123,5 +127,38 @@ void afficher_tableau_char(char a[N][N]){
             printf("%c ", a[i][j]);
         }
         printf("\n");
+    }
+}
+void decouvrir_mines(int tableau_complet[N][N], char tableau_user[N][N]){
+    for(int i = 0; i < N;i++){
+        for(int j = 0; j < N;j++){
+            if(tableau_complet[i][j]==9) tableau_user[i][j] = 'M';
+            if(tableau_complet[i][j]==1) tableau_user[i][j] = '1';
+            if(tableau_complet[i][j]==2) tableau_user[i][j] = '2';
+            if(tableau_complet[i][j]==3) tableau_user[i][j] = '3';
+            if(tableau_complet[i][j]==4) tableau_user[i][j] = '4';
+            if(tableau_complet[i][j]==5) tableau_user[i][j] = '5';
+            if(tableau_complet[i][j]==6) tableau_user[i][j] = '6';
+            if(tableau_complet[i][j]==7) tableau_user[i][j] = '7';
+            if(tableau_complet[i][j]==8) tableau_user[i][j] = '8';
+            if(tableau_complet[i][j]==0) tableau_user[i][j] = '0';
+
+        }
+    }
+}
+void decouvrir(int tableau_complet[N][N], char tableau_user[N][N], int a, int b){
+    //printf("%c", tableau_user[a][b]);
+    if(tableau_user[a][b] == '0' || tableau_user[a][b] == 'X'){
+        if(tableau_complet[a][b] == 0){
+            decouvrir(tableau_complet, tableau_user, a - 1, b + 0);
+            decouvrir(tableau_complet, tableau_user, a + 1, b + 0);
+            decouvrir(tableau_complet, tableau_user, a + 0, b + 1);
+            decouvrir(tableau_complet, tableau_user, a + 0, b - 1);
+            decouvrir(tableau_complet, tableau_user, a + 1, b + 1);
+            decouvrir(tableau_complet, tableau_user, a + 1, b - 1);
+            decouvrir(tableau_complet, tableau_user, a - 1, b + 1);
+            decouvrir(tableau_complet, tableau_user, a - 1, b - 1);
+
+        }
     }
 }
